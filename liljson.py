fd1181c8dce71f8fe687008a10a34984c222437a
straup <straup@gmail.com>
2012-08-19 09:12:15 -0400

    move the writing part in to a function so it can be used from other tools/scripts

--- liljson.py
+++ liljson.py
@@ -3,45 +3,22 @@ from json import load, JSONEncoder
 from optparse import OptionParser
 from re import compile
 
+defaults = dict(precision=3)
+
 float_pat = compile(r'^-?\d+\.\d+(e-?\d+)?$')
 charfloat_pat = compile(r'^[\[,\,]-?\d+\.\d+(e-?\d+)?$')
 
-parser = OptionParser(usage="""%prog [options]
+def write_liljson(data, output, precision):
 
-Make JSON smaller by reducing precision of floating point values
-and removing white space. Works great for GeoJSON!
+    args = { 'separators': (',', ':') }
 
-Examples:
-  cat input.json | python %prog --precision 2 > output.json
-  python %prog --precision 2 input.json > output.json
-  python %prog -p 2 input.json output.json""")
-
-defaults = dict(precision=3)
-
-parser.set_defaults(**defaults)
-
-parser.add_option('-p', '--precision', dest='precision',
-                  type='int', help='Digits of precision, default %(precision)d.' % defaults)
-
-if __name__ == '__main__':
-    options, args = parser.parse_args()
-    
-    #
-    # Read!
-    #
-    input = len(args) and open(args[0]) or stdin
-    data = load(input)
-    
-    #
-    # Write!
-    #
-    encoder = JSONEncoder(separators=(',', ':'))
+    encoder = JSONEncoder(**args)
     encoded = encoder.iterencode(data)
     
-    format = '%.' + str(options.precision) + 'f'
-    output = len(args) == 2 and open(args[1], 'w') or stdout
+    format = '%.' + str(precision) + 'f'
     
     for token in encoded:
+
         if charfloat_pat.match(token):
             # in python 2.7, we see a character followed by a float literal
             output.write(token[0] + format % float(token[1:]))
@@ -52,3 +29,33 @@ if __name__ == '__main__':
 
         else:
             output.write(token)
+
+if __name__ == '__main__':
+
+    parser = OptionParser(usage="""%prog [options]
+
+Make JSON smaller by reducing precision of floating point values
+and removing white space. Works great for GeoJSON!
+
+Examples:
+  cat input.json | python %prog --precision 2 > output.json
+  python %prog --precision 2 input.json > output.json
+  python %prog -p 2 input.json output.json""")
+
+    parser.set_defaults(**defaults)
+
+    parser.add_option('-p', '--precision', dest='precision',
+                      type='int', help='Digits of precision, default %(precision)d.' % defaults)
+
+    options, args = parser.parse_args()
+    
+    #
+    # Read!
+    #
+
+    input = len(args) and open(args[0], 'r') or stdin
+    output = len(args) >= 2 and open(args[1], 'w') or stdout
+
+    data = load(input)
+
+    write_liljson(data, output, options.precision)
